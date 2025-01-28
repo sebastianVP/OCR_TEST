065fce055086f8369380c9f16fba7e798434a49c
sebastianVP <alex.valdezp22@gmail.com>
2025-01-28 18:36:06 -0500

    script de prueba

---
+++ test_ocr.py
@@ -0,0 +1,16 @@
+from PIL import Image
+import pytesseract
+
+# Cargar la imagen
+image_path = "Captura_001.PNG"  # Cambia por la ruta de tu imagen
+#image_path = "Captura2.PNG"  # Cambia por la ruta de tu imagen
+#image_path = "imagenes_red_n.PNG"  # Cambia por la ruta de tu imagen
+
+image = Image.open(image_path)
+
+# Realizar OCR
+texto_extraido = pytesseract.image_to_string(image, lang="spa")  # 'spa' para español
+
+# Imprimir el resultado
+print("Texto extraído:")
+print(texto_extraido)
